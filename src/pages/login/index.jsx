825c77a43e31e5700f0ed8f3eef3840fdb5470b5
baongoc8548 <locphan390@gmail.com>
2022-06-05 19:37:15 +0700

    create UI for Register

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
 import Navbar from "../../components/navbar";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+const fakeAccount={username: "admin", password: "admin"}
+
 export default function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const handleChange = (val,field) => {
+   
+    if(field==="username"){
+      setUsername(val)
+      console.log("Username: ",val);
+    }
+    else{
+      setPassword(val)
+      console.log("Password: ",val);
+    }
+  };
+  const hanldeLogin=()=>{
+    if(username===fakeAccount.username && password===fakeAccount.password){
+      console.log("DANG NHAP THANH CONG!");
+      navigate('/')
+
+    }
+    else{
+      console.log("DANG NHAP KHONG THANH CONG!")
+      alert("Tên tài khoản hoặc mật khẩu không chính xác. vui lòng thử lại!!!")
+    }
+  }
   return (
     <div className="loginContainer">
       <Navbar />
@@ -18,7 +46,7 @@ export default function Login() {
               Username
             </Form.Label>
             <Col sm="9" className="">
-              <Form.Control type="text" placeholder="Username" />
+              <Form.Control type="text" placeholder="Username" value={username} onChange={(e)=>handleChange(e.target.value,"username")} />
             </Col>
           </Form.Group>
 
@@ -31,14 +59,15 @@ export default function Login() {
               Password
             </Form.Label>
             <Col sm="9">
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e)=>handleChange(e.target.value,"password")}/>
             </Col>
           </Form.Group>
           <div className="d-flex justify-content-center mt-4">
-            <Button variant="success" className="">
+            <Button variant="success" className="" onClick={hanldeLogin}>
               Đăng nhập
             </Button>{" "}
           </div>
+          <p className="create" onClick={()=>navigate("/register")}>Tạp tài khoản mới</p>
         </Form>
       </div>
     </div>
---
+++ src/pages/register/index.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { Form, Col, Row, Button, InputGroup } from "react-bootstrap";
+import Navbar from "../../components/navbar";
+import "./style.scss";
+import { useNavigate } from "react-router-dom";
+export default function Register() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const handleChange = (val, field) => {
+    if (field === "username") {
+      setUsername(val);
+    }
+    else if (field === "password") {
+      setPassword(val);
+    } else {
+      setConfirmPassword(val);
+      console.log("PASSWORD:", val);
+    }
+  };
+  const handleRegister = () => {
+    alert("tạo tài khoản thành công!");
+
+    navigate("/login");
+  };
+
+  return (
+    <div className="RegisterContainer">
+      <Navbar />
+      <div className="formContainer">
+        <h2>Đăng kí</h2>
+        <Form>
+          <Form.Group
+            as={Row}
+            className="mb-3 d-flex justify-content-between"
+            controlId="formPlaintextUsername"
+          >
+            <Form.Label column sm="2">
+              Username
+            </Form.Label>
+            <Col sm="9" className="">
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => handleChange(e.target.value, "username")}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3 d-flex justify-content-between"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => handleChange(e.target.value, "password")}
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group
+            as={Row}
+            className="mb-3 d-flex justify-content-between"
+            controlId="formPlaintextPassword"
+          >
+            <Form.Label column sm="2">
+              Nhập lại Password
+            </Form.Label>
+            <Col sm="9">
+              <Form.Control
+                type="password"
+                placeholder="Nhập lại Password"
+                value={confirmPassword}
+                onChange={(e) => handleChange(e.target.value, "confirmPassword")}
+              />
+            </Col>
+          </Form.Group>
+          <div>
+            <InputGroup className="mb-3">
+              <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+              remember password
+            </InputGroup>
+          </div>
+          <div className="d-flex justify-content-center mt-4">
+            <Button variant="success" className="" onClick={handleRegister}>
+              Đăng kí
+            </Button>{" "}
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
